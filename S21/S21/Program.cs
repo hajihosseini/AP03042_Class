f92e8426f785e3b957faba3b49cd9516688f7c69
Malihe Hajihosseini <m.hajihosseini95@gmail.com>
2025-05-05 09:12:28 +0330

    s21

---
+++ S21/S21/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace S21;
+
+class Program
+{
+    public static IEnumerable<string> GetNames()
+    {
+        yield return "ali";
+        yield return "fateme";
+        yield return "zahra";
+    }
+
+
+    static void Main(string[] args)
+    {
+
+    }
+
+    
+    static void Main1(string[] args)
+    {
+        foreach(var item in GetNames())
+            System.Console.WriteLine(item);
+        // Console.WriteLine("Hello, World!");
+    }
+}
