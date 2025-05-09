1eaa57d6701ae8f45399861b5e4c049734dbcb6f
Malihe Hajihosseini <m.hajihosseini95@gmail.com>
2025-05-09 18:01:05 +0330

    update s21

--- S21/S21/Program.cs
+++ S21/S21/Program.cs
@@ -1,5 +1,33 @@
 ﻿namespace S21;
 
+public static class EXT
+{
+    public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
+    {
+        List<T> values = new List<T>();
+        foreach(var item in source)
+        {
+            if(values.Count()<count)
+                values.Add(item);
+            else
+            {
+                break;
+            }
+        }
+        return values;
+    }
+
+    public static IEnumerable<T> MyTake2<T>(this IEnumerable<T> source, int count)
+    {
+        foreach(var item in source) 
+        {
+            if(count-->0)
+                yield return item;
+        }
+    }
+}
+
+
 class Program
 {
     public static IEnumerable<string> GetNames()
@@ -12,10 +40,67 @@ class Program
 
     static void Main(string[] args)
     {
+        var data = File.ReadAllLines("children-per-woman-UN.csv")
+                        .Skip(1)
+                        .Select(l=>
+                            {
+                                string[] tokens = l.Split(',');
+                                string country=tokens[0];
+                                int year = int.Parse(tokens[2]);
+                                double rate = double.Parse(tokens[3]);
+                                return (country:country, year:year, rate:rate);
+                             });
+
+        data.Join(data,
+                 (d1)=>(d1.country,d1.year),
+                 (d2)=>(d2.country,d2.year-1),
+                 (d1,d2)=>(country:d1.country, y1:d1.year, y2:d2.year, r1:d1.rate, r2:d2.rate)
+        )
+        .GroupBy(t=>t.country)
+        .Select(g=>
+        {
+            var maxdiff = g.MaxBy(t=>Math.Abs(t.r2-t.r1));
+            return (country:g.Key, diff:Math.Abs(maxdiff.r2-maxdiff.r1));
+        }
+        )
+        .OrderBy(t=>t.diff)
+        .ToList()
+        .ForEach(l=>System.Console.WriteLine((l)));
+
+                
 
     }
 
-    
+    static void Main2(string[] args)
+    {
+        //string[] lines = 
+        File.ReadAllLines("children-per-woman-UN.csv")
+                            .Skip(1)
+                            //.Where(l=>l.ToLower().Contains("iran"))
+                            .Select(l=>
+                            {
+                                string[] tokens = l.Split(',');
+                                string country=tokens[0];
+                                int year = int.Parse(tokens[2]);
+                                double rate = double.Parse(tokens[3]);
+                                return (country:country, year:year, rate:rate);
+                             })
+                            .GroupBy(t=>t.country)
+                            .Select(g=>
+                            {
+                                return (country:g.Key, rate:g.Average(t=>t.rate));
+                            })
+                            //.Where(t=>(t.year>2012)&&(t.rate>3))
+                            .OrderBy(t=>t.rate)
+                            //.Take(5)
+                            .ToList()
+                            .ForEach(l=>System.Console.WriteLine(l));
+
+        // foreach(var item in lines.MyTake2(10))
+        //     System.Console.WriteLine(item);
+    }
+
+
     static void Main1(string[] args)
     {
         foreach(var item in GetNames())
