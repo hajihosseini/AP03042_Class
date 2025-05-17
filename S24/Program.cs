57ba94689357dd8427310b849dfee519c20055bf
Sauleh Eetemadi <sauleh@gmail.com>
2025-05-17 10:57:04 +0400

    update

--- S24/Program.cs
+++ S24/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace S24;
+﻿using System.Diagnostics;
+
+namespace S24;
 
 class Program
 {
@@ -6,13 +8,52 @@ class Program
     {
         int n = 15;
         double d = n;
-        for (int i = 0; i < 100_000; i++)
+        for (int i = 0; i < 100_000_000; i++)
+            d = d * Math.Sqrt(n) % n;
+        return d;
+    }
+
+
+    static async Task<double> CPUIntensiveAsync()
+    {        
+        int n = 15;
+        double d = n;
+        for (int i = 0; i < 100_000_000; i++)
             d = d * Math.Sqrt(n) % n;
         return d;
     }
 
     static void Main(string[] args)
     {
-        Task<double> t = new Task<double>()
+        CPUIntensiveAsync().Start();
+        
+        Stopwatch sw = Stopwatch.StartNew();
+        double sum = 0;
+        for (int i = 0; i < 20; i++)
+            sum += CPUIntensive();
+        sw.Stop();
+        System.Console.WriteLine(sw.Elapsed.ToString());
+        System.Console.WriteLine(sum);
+
+    }
+
+
+    static void Main2(string[] args)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        Task<double>[] tasks = new Task<double>[20];
+        for (int i = 0; i < 20; i++)
+        {
+            tasks[i] = new Task<double>(CPUIntensive);
+            tasks[i].Start();
+        }
+        Task.WaitAll(tasks);
+        double sum = 0;
+        for (int i = 0; i < 20; i++)
+            sum += tasks[i].Result;
+
+        System.Console.WriteLine(sum);
+        sw.Stop();
+        System.Console.WriteLine(sw.Elapsed.ToString());
     }
 }
