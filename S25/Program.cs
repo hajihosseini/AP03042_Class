85869549d877fd17d1e3ec99888401205d2708b5
Sauleh Eetemadi <sauleh@gmail.com>
2025-05-19 10:07:20 +0400

    update

---
+++ S25/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace S25;
+
+class Program
+{
+    static double dowork(object obj)
+    {
+        int n = (int)obj;
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} => {n}");
+        double d = 1;
+        for (int i = 1; i < n; i++)
+            d = (d * i) % int.MaxValue;
+        return d;
+    }
+
+    static void Main(string[] args)
+    {
+        var tasks = new Task<double>[19];
+        double sum = 0;
+        for (int i = 1; i < 20; i++)
+        {
+            tasks[i - 1] = Task<double>.Factory.StartNew(dowork, i * 10_000_000);
+        }
+        Task.WaitAll(tasks);
+
+        foreach (var t in tasks)
+            sum += t.Result;
+        System.Console.WriteLine(sum);
+    }
+
+    static void Main3(string[] args)
+    {
+        var tasks = new Task<double>[19];
+        double sum = 0;
+        for (int i = 1; i < 20; i++)
+        {
+            tasks[i - 1] = Task<double>.Factory.StartNew(dowork, i * 10_000_000);
+        }
+        Task.WaitAll(tasks);
+
+        foreach (var t in tasks)
+            sum += t.Result;
+        System.Console.WriteLine(sum);
+    }
+
+    static void Main2(string[] args)
+    {
+        var t = Task.Run(() => dowork(10_000_000));
+        while (true)
+        {
+            if (t.Status == TaskStatus.RanToCompletion)
+                break;
+            System.Console.WriteLine("Waiting");
+            Task td = Task.Delay(10);
+            td.Wait();
+        }
+        System.Console.WriteLine(t.Result);
+    }
+}
---
+++ S25/Program2.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace S25;
+
+class Program33
+{
+    static async Task Main33(string[] args)
+    {
+        using var client = new HttpClient();
+        string result = await client.GetStringAsync("https://www.tabnak.ir/");
+        string pattern = @"(?:src)\s*=\s*[""'](?<url>https?://[^""']+)[""']";
+        foreach (Match match in Regex.Matches(result, pattern, RegexOptions.IgnoreCase))
+        {
+            try
+            {
+                string url = match.Groups["url"].Value;
+                var bytes = client.GetByteArrayAsync(url).Result;
+                string filename = Path.GetFileName(url);
+                File.WriteAllBytes(filename, bytes);
+                Console.WriteLine(filename + " => " + url);
+            }
+            catch (System.AggregateException ae)
+            {
+                System.Console.WriteLine(ae.Message);
+            }
+        }        
+    }
+}
