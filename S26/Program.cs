30b26de7a6ae1aa0500901e705b09bf4e9d4a743
Sauleh Eetemadi <sauleh@gmail.com>
2025-05-24 10:59:22 +0400

    u

--- S26/Program.cs
+++ S26/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Collections.Concurrent;
 
 namespace S26;
 // Paralle.For
@@ -9,7 +10,34 @@ namespace S26;
 
 class Program
 {
-    static bool  IsPrime(int n)
+    static IEnumerable<int> GetNums()
+    {
+        System.Console.WriteLine("I'm here!!!");
+        for (int i = 0; i >= 0; i++)
+            yield return i;
+    }
+
+    static void Main5534(string[] args)
+    {
+        var nums = GetNums();
+        foreach (var n in nums.Take(5))
+            System.Console.WriteLine(n);
+    }
+
+    
+    static void Main4545454(string[] args)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        var pnums =
+            Enumerable.Range(2, MAX)
+                    .GroupBy(n => Random.Shared.Next(1, 100))
+                    .AsParallel()
+                    .SelectMany(g => g.Where(n => IsPrime(n)));
+        System.Console.WriteLine(pnums.Count());
+        System.Console.WriteLine(sw.Elapsed.ToString());
+    }
+
+    static bool IsPrime(int n)
     {
         for (int i = 2; i <= Math.Sqrt(n); i++)
             if (n % i == 0)
@@ -17,7 +45,7 @@ class Program
         return true;
     }
     const int MAX = 20_000_000;
-    static void Main(string[] args)
+    static void Main2(string[] args)
     {
         Stopwatch sw = Stopwatch.StartNew();
         int count = 0;
@@ -32,14 +60,47 @@ class Program
         System.Console.WriteLine(count);
     }
 
-    static void Main2(string[] args)
+    static StreamWriter _writer = null;
+    static object mylock = new object();
+    static StreamWriter writer
+    {
+        get
+        {
+            if (_writer == null)
+            {
+                lock (mylock)
+                {
+                    if (_writer == null)
+                        _writer = new StreamWriter("primes.txt");
+                }
+            }
+            return _writer;
+
+        }
+    }
+
+    static void Main(string[] args)
     {
         Stopwatch sw = Stopwatch.StartNew();
         int count = 0;
-        var pflr = Parallel.For(2, MAX, i =>
+        // List<int> prime_nums = new List<int>();
+        // ConcurrentBag<int> prime_nums = new ConcurrentBag<int>();
+        Parallel.For(2, MAX, i =>
         {
+
             if (IsPrime(i))
+            {
                 Interlocked.Increment(ref count);
+                lock (sw) writer.WriteLine($"{i}");
+            }
+
+
+
+
+            // lock(prime_nums)
+            // prime_nums.Add(i);
+
+            // Interlocked.Increment(ref count);
         });
         System.Console.WriteLine(sw.Elapsed.ToString());
         System.Console.WriteLine(count);
