3ac30e54288742831e3a3954b16e4769645f8b83
Sauleh Eetemadi <sauleh@gmail.com>
2025-05-31 10:44:40 +0400

    u

--- S28/CoRP.cs
+++ S28/CoRP.cs
@@ -13,8 +13,16 @@ public abstract class CashDispenser
         if (amount2dispense > 0)
             System.Console.WriteLine($"{BankNoteUnit}Dispenser: Here you are: ${amount2dispense}");
 
+        amount2dispense = amount2dispense % BankNoteUnit;
         if (_nextCashDispenser != null)
-            _nextCashDispenser.Dispense(amount2dispense % BankNoteUnit);
+        {
+            if (amount2dispense > 0)
+                _nextCashDispenser.Dispense(amount2dispense);
+        }
+        else if (amount2dispense > 0)
+        {
+            System.Console.WriteLine($"ERROR: Cannot dipsense this amount: {amount}");
+        }
     }
 }
 
